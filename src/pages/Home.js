1f76bd381b7bb2b30092973039e267d5c15d8d0f
younaj00 <you.na.joo0@gmail.com>
2025-03-28 10:28:21 +0900

    feat: Main page 구성

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Chart from './../components/Chart';
+import ExchangeTable from './../components/ExchangeTable';
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className=''>
+      container mx-auto px-4 py-8
+      <h1 className='text-3xl text-center font-bold mb-4'>실시간 차트</h1>
+      <p className='text-center text-gray-300 mb-6'>2025.03.28 12:03</p>
+      <Chart/>
+      <ExchangeTable/>
+    </div>
+  );
 }
 
 export default Home
